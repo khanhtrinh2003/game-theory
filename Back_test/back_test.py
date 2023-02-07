ebde3e2370026723d5a3f71cda5455567008e475
khanhtrinh2003 <nguyenduckhanhtrinh@gmail.com>
2023-02-07 22:27:09 +0700

    update

--- Back_test/back_test.py
+++ Back_test/back_test.py
@@ -3,16 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-Peo1 = ["D1", "D2",  "D3",  "D4",  "D5",  "D6"]
-Peo2 = ["V1", "V2",  "V3",  "V4",  "V5",  "V6"]
-payoff1 = [[0,1,1,2,1,1],[1,0,2,1,1,1],[1,1,0,1,1,2],[1,1,1,0,2,1],[2,1,1,1,0,1],[1,2,1,1,1,0]]
-payoff2 = [[3,2,2,1,2,2],[2,3,1,2,2,2],[2,2,3,2,2,1],[2,2,2,3,1,2],[1,2,2,2,3,2],[2,1,2,2,2,3]]
-dic = {
-    "D1":0, "D2":1,  "D3":2,  "D4":3,  "D5":4,  "D6":5,
-    "V1":0, "V2":1,  "V3":2,  "V4":3,  "V5":4,  "V6":5
-}
-
-
 class Kiem_dinh():
     def __init__(self, 
                 so_chien_luoc1, 
@@ -44,11 +34,11 @@ class Kiem_dinh():
             chien_luoc = [list(e) for e in zip(s1, s2)]
             u1.append(sum(list(map(self.thu_hoach1, chien_luoc))))
             u2.append(sum(list(map(self.thu_hoach2, chien_luoc))))                        
-        u = pd.DataFrame({f"Lợi ích của {Kiem_dinh.ten[0]}": u1,f"Lợi ích của {Kiem_dinh.ten[1]} ": u2})
+        u = pd.DataFrame({f"Lợi ích của người 1": u1,f"Lợi ích của người 2": u2})
         
         fig= plt.subplots(1,1)
-        u[f"Lợi ích của {Kiem_dinh.ten[0]}"].plot(kind='density')
-        u[f"Lợi ích của {Kiem_dinh.ten[1]} "].plot(kind='density')
+        u[f"Lợi ích của người 1"].plot(kind='hist')
+        u[f"Lợi ích của người 2"].plot(kind='hist')
         plt.legend()
         plt.xlabel("Lợi ích thu được được")
         plt.savefig(fname='test')
@@ -60,10 +50,3 @@ class Kiem_dinh():
     def thu_hoach2(self,s):
         return self.payoff2[s[0]][s[1]]
 
-
-class Game_theo(Kiem_dinh):
-    def __init__(self, p, ten):
-        super().__init__(p, ten)
-
-
-Game_theo(0.2,"a").mat_do_loi_nhuan()
